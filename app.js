5c4862cbd19d3209abe7ec03789bc1b520a36560
Deepak Bhagat <deepak2014bhagat.db@gmail.com>
2021-10-20 20:41:18 +0530

    passport.js

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const passport = require('passport');
 //const encryption = require("mongoose-encryption");  //(level 2 security)
 //const md5 = require("md5");             //level 4 security (hashing)
 //const bcrypt = require("bcrypt");          // level 5 security (salting)
 //const  saltRounds = 10;
-
+const session = require('express-session'); //level 7 security (passport)
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 
@@ -21,16 +22,31 @@ const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(session({
+    secret : "Our little secret.",
+    resave : false,
+    saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema =  new mongoose.Schema({
     email : String,
     password : String
 });
+
+userSchema.plugin(passportLocalMongoose);
 // level 2 security using moongoose encryption 
 
 //userSchema.plugin(encryption, {secret: process.env.SECRET, encryptedFields : ["password"]}); // level 3 security using(.env)
 const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -44,8 +60,37 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+
+
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login")
+    }
+});
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
 app.post("/register", function (req, res) {
 
+User.register({username : req.body.username}, req.body.password, function (err, user){
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
+});
+
+
+
+    /*  commenting out whole code  for using passport
     bcrypt.hash(req.body.password, saltRounds, function (err, hash) {    //security level 5 using hashing
         const newUser = new User({
             email : req.body.username,
@@ -58,12 +103,32 @@ app.post("/register", function (req, res) {
                 console.log(err);
             }
         });
-    });
+    });*/
     
 });
 
 app.post("/login", function (req, res) {
-  
+
+const user = new User({
+    username : req.body.username,
+    password : req.body.password
+});
+
+req.login(user, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/secrets");
+        });
+    }
+});
+
+
+
+
+
+  /*  commenting out whole code  for using passport
     const username = req.body.username;
     const password = req.body.password;                                              //md5(req.body.password); no longer required md5 part
 // level 1 sceurity- using email and password
@@ -82,7 +147,7 @@ app.post("/login", function (req, res) {
                 }
             }
         
-    });
+    });**/
 });
 
 
